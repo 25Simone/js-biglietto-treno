9edb9edf12e412b0b61a89e832a61e3e3ff0c7a9
25Simone <dantsimone111@gmail.com>
2021-12-03 13:31:06 +0100

    struttura html-js e impostazione esercizio

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// creo la variabile collegata all'id html
+const outputHtml = document.getElementById('output');
+
+// chiedo all'utenete il numero di chilometri che vuole percorrere
+let km = prompt('Inserisci qui il numero di km da percorre.')
+
+// chiedo all'utente l'età del passeggero
+let age = prompt("Inersici qui l'eta del passeggero.")
+
+// calcolo il prezzo totale del viaggio in base alle condizioni date
