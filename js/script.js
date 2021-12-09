28bad7635b3e9e60ac279d286e578315060d3bc1
25Simone <dantsimone111@gmail.com>
2021-12-09 12:44:29 +0100

    Aggiunta del button che permette al'utente di calcolare il prezzo del biglietto

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
+// Costante che definisce il rezzo per chilometro
+const priceForKm = 0.21;
 // creo la variabile collegata all'id html
 const outputHtml = document.getElementById('output');
 
 // chiedo all'utenete il numero di chilometri che vuole percorrere
-let km = prompt('Inserisci qui il numero di km da percorre.');
+let km = parseInt(prompt('Inserisci qui il numero di km da percorre.'));
 document.getElementById('user-km').innerHTML = `${km}`;
 
 // chiedo all'utente l'età del passeggero
-let age = prompt("Inersici qui l'eta del passeggero.");
+let age = parseInt(prompt("Inersici qui l'eta del passeggero."));
 document.getElementById('user-age').innerHTML = `${age}`;
 
 // prezzo del biglietto
-let price = km * 0.21;
+let price = km * priceForKm;
 
 // definisco gli eventuali sconti
 let discount20 = price * 0.20;
@@ -19,20 +21,27 @@ let discount40 = price * 0.40;
 // definisco la variabile del prezzo finale
 let finalPrice;
 
-// discount 20% per minorenni
-if (age < 18) {
-    finalPrice = price - discount20;
-} else if (age > 65) {
-    finalPrice = price - discount40;
-} else {
-    finalPrice = price;
-}
+// dichiaro la variabile del button che permetterà all'utente il calcolo del prezzo
+const button = document.getElementById("button");
 
-// dichiaro la variabile del prezzo arrotondato a due cifre decimali
-let finalPriceRounded = Math.round((finalPrice + Number.EPSILON) * 100) / 100;
+// aggiungo l'evento al click del bottone
+button.addEventListener("click", function() {
+     // discount
+    if (age < 18) {
+        finalPrice = price - discount20;
+    } else if (age > 65) {
+        finalPrice = price - discount40;
+    } else {
+        finalPrice = price;
+    }
 
-// stampo il risultato
-outputHtml.innerHTML += `${finalPriceRounded} &euro;`;
+    // dichiaro la variabile del prezzo arrotondato a due cifre decimali
+    let finalPriceRounded = parseFloat(finalPrice.toFixed(2));
+
+    // stampo il risultato
+    outputHtml.innerHTML += `${finalPriceRounded} &euro;`;
+})
+
 
 // definisco la data di validazione del ticket
 const dateHtml = document.getElementById('validation-date');
